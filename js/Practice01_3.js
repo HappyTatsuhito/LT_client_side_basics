52112a3af6aea4c4347530582f31b31ec45e2271
HappyTatsuhito <c6100166@planet.kanazawa-it.ac.jp>
2021-05-13 19:05:01 +0900

    完成版

--- js/Practice01_3.js
+++ js/Practice01_3.js
@@ -2,29 +2,41 @@ var flg1=false, flg2=false, flg3=false;
 var rand1 = Math.floor(Math.random() * 10);
 var rand2 = Math.floor(Math.random() * 10);
 var rand3 = Math.floor(Math.random() * 10);
+var arrChangeCnt = new Array();
+var intervalID;
 
 function StartButtonClick(){
     flag1=false, flag2=false, flag3=false;
-    while(!flag1 || !flag2 || !flag3){
-        if(!flag1){rand1 = Math.floor(Math.random() * 10);}
-        if(!flag2){rand2 = Math.floor(Math.random() * 10);}
-        if(!flag3){rand3 = Math.floor(Math.random() * 10);}
+    arrChangeCnt.push(intervalID = setInterval(RandomSlot, 100));
+}
+
+function RandomSlot(){
+    if(!flag1){
+        rand1 = Math.floor(Math.random() * 10);
+        innerRand1.innerHTML = rand1;
+    }
+    if(!flag2){
+        rand2 = Math.floor(Math.random() * 10);
+        innerRand2.innerHTML = rand2;
+    }
+    if(!flag3){
+        rand3 = Math.floor(Math.random() * 10);
+        innerRand3.innerHTML = rand3;
     }
 }
 
 function StopButtonClick1(){
     flag1=true;
-    rand1 = Math.floor(Math.random() * 10);
     StopChecker();
 }
+
 function StopButtonClick2(){
     flag2=true;
-    rand2 = Math.floor(Math.random() * 10);
     StopChecker();
 }
+
 function StopButtonClick3(){
     flag3=true;
-    rand3 = Math.floor(Math.random() * 10);
     StopChecker();
 }
 
@@ -32,6 +44,7 @@ function StopChecker(){
     if(!((flag1 == flag2) && (flag1== flag3))){
         return;
     }
+    clearInterval(arrChangeCnt.shift());
     if((rand1 == rand2) && (rand1 == rand3)){
         alert("あたり");
     }
